82e8b8b58bb7444b859633b3963872cf5100e64a
khj1645 <khj164552@gmail.com>
2026-03-21 20:13:06 +0900

    서버 쪽 코드 넣기

---
+++ gaym/ServerCore/CoreGlobal.cpp
@@ -0,0 +1,47 @@
+#include "pch.h"
+#include "CoreGlobal.h"
+#include "ThreadManager.h"
+#include "Memory.h"
+#include "DeadLockProfiler.h"
+#include "SocketUtils.h"
+#include "SendBuffer.h"
+#include "GlobalQueue.h"
+#include "JobTimer.h"
+#include "ConsoleLog.h"
+
+ThreadManager*		GThreadManager = nullptr;
+Memory*				GMemory = nullptr;
+SendBufferManager*	GSendBufferManager = nullptr;
+GlobalQueue*        GGlobalQueue = nullptr;
+JobTimer*           GJobTimer = nullptr;
+
+DeadLockProfiler*	GDeadLockProfiler = nullptr;
+ConsoleLog*         GConsoleLogger = nullptr;
+
+class CoreGlobal
+{
+public:
+	CoreGlobal()
+	{
+		GThreadManager = new ThreadManager();
+		GMemory = new Memory();
+		GSendBufferManager = new SendBufferManager();
+		GGlobalQueue = new GlobalQueue();
+		GJobTimer = new JobTimer();
+		GDeadLockProfiler = new DeadLockProfiler();
+		GConsoleLogger = new ConsoleLog();
+		SocketUtils::Init();
+	}
+
+	~CoreGlobal()
+	{
+		delete GThreadManager;
+		delete GMemory;
+		delete GSendBufferManager;
+		delete GGlobalQueue;
+		delete GJobTimer;
+		delete GDeadLockProfiler;
+		delete GConsoleLogger;
+		SocketUtils::Clear();
+	}
+} GCoreGlobal;
---
+++ gaym/ServerCore/Memory.h
@@ -0,0 +1,61 @@
+#pragma once
+#include "Types.h"
+#include "Allocator.h"
+#include <vector>
+#include <memory>
+#include <utility>
+using std::vector;
+using std::shared_ptr;
+using std::forward;
+
+class MemoryPool;
+
+/*-------------
+	Memory
+---------------*/
+
+class Memory
+{
+	enum
+	{
+		// ~1024���� 32����, ~2048���� 128����, ~4096���� 256����
+		POOL_COUNT = (1024 / 32) + (1024 / 128) + (2048 / 256),
+		MAX_ALLOC_SIZE = 4096
+	};
+
+public:
+	Memory();
+	~Memory();
+
+	void*	Allocate(int32 size);
+	void	Release(void* ptr);
+
+private:
+	vector<MemoryPool*> _pools;
+
+	// �޸� ũ�� <-> �޸� Ǯ
+	// O(1) ������ ã�� ���� ���̺�
+	MemoryPool* _poolTable[MAX_ALLOC_SIZE + 1];
+};
+
+
+template<typename Type, typename... Args>
+Type* xnew(Args&&... args)
+{
+	Type* memory = static_cast<Type*>(PoolAllocator::Alloc(sizeof(Type)));
+	new(memory)Type(forward<Args>(args)...); // placement new
+	return memory;
+}
+
+template<typename Type>
+void xdelete(Type* obj)
+{
+	obj->~Type();
+	PoolAllocator::Release(obj);
+}
+
+template<typename Type, typename... Args>
+shared_ptr<Type> MakeShared(Args&&... args)
+{
+	return shared_ptr<Type>{ xnew<Type>(forward<Args>(args)...), xdelete<Type> };
+}
